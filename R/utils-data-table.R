c72fb539445b6093a72f0140d4840a8790f4a18e
pottj <pott.ne.ja@gmail.com>
2021-10-21 17:40:08 +0200

    fixing data.table

--- R/utils-data-table.R
+++ R/utils-data-table.R
@@ -9,4 +9,10 @@
 # advice (vignette('datatable-importing', 'data.table')).
 #
 #' @import data.table
-NULL
+#' @importFrom data.table ":="
+
+# Make sure data.table knows we know we're using it
+.datatable.aware = TRUE
+
+# standard data.table variables
+if (getRversion() >= "2.15.1")  utils::globalVariables(c(".", ".I", ".N", ".SD"), utils::packageName())
