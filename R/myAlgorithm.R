e6df34393b57e555a77e91c1d39fcc44b943fa34
pottj <pott.ne.ja@gmail.com>
2021-10-22 14:50:33 +0200

    Testing algorithm

--- R/myAlgorithm.R
+++ R/myAlgorithm.R
@@ -1,8 +1,9 @@
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param c PARAM_DESCRIPTION
-#' @return OUTPUT_DESCRIPTION
-#' @details DETAILS
+#' @title Algorithm to find fixing taxa
+#' @description Split data to input and unresolved quadruples and try for each unresolved one if a fixing taxon can be found.
+#' @param data Data.table as constructed by create input + count and fixing taxon. All possible quadruples given the taxon set with status information (quadruple as input available, quadruple not in input = unresolved, quadruples already solved = resolved). In addition, all four triples possible by each quadruple are listed. For resolved quadruples, used fixing taxon and round of resolvement is listed.
+#' @param verbose Logical parameter if message should be printed; default F
+#' @return The same data.table is returned, with updated status, fixing taxon & round
+#' @details Details to the algorithm can be found in the paper LINK
 #' @examples
 #' \dontrun{
 #' if(interactive()){
@@ -11,38 +12,78 @@
 #' }
 #' @rdname myAlgorithm
 #' @export
-myAlgorithm<-function(c){
-  # a = alt_green
-  # b = alt_cross
-  # c = all_quads
+#' @importFrom foreach foreach
+#' @importFrom foreach "%do%"
+#' @importFrom data.table ":="
+myAlgorithm<-function(data,verbose = F){
+  # data = data2
 
-  filt_unresolved<-c$status == "unresolved"
-  b<-c[filt_unresolved,]
-  a<-c[!filt_unresolved,]
-  y<-vector(mode="numeric",length=dim(b)[1])
-  for (i in c(1:dim(b)[1])){
-    triples_CQ<-t(b[i,c(6,7,8,9)]);
-    vgl1<-is.element(a$triple1,triples_CQ);
-    vgl2<-is.element(a$triple2,triples_CQ);
-    vgl3<-is.element(a$triple3,triples_CQ);
-    vgl4<-is.element(a$triple4,triples_CQ);
-    filt<-vgl1 | vgl2 | vgl3 | vgl4 ;
-    pos<-a[filt,];
-    x<-c(pos$taxa1,pos$taxa2,pos$taxa3,pos$taxa4);
-    filt_x1<- x == b[i,2];
-    filt_x2<- x == b[i,3];
-    filt_x3<- x == b[i,4];
-    filt_x4<- x == b[i,5];
-    filt_x<- filt_x1 | filt_x2 | filt_x3 | filt_x4;
-    x1<-x[!filt_x];
-    y[i]<-max(table(x1))
+  # Step 0: check if input is ok
+  expectedNames = c("taxa1","taxa2","taxa3","taxa4","quadruple",
+                    "triple1","triple2","triple3","triple4",
+                    "status","round","fixingTaxa")
+  stopifnot(names(data) %in% expectedNames)
+  stopifnot(class(data$taxa1) == "integer")
+
+  # Step 1: split quadruples in resolved and unresolved ones
+  data_solved = data[status != "unresolved"]
+  data_unresolved = data[status == "unresolved"]
+
+  # Step 2: check every unresolved quadruple for fixing taxon
+  myTab = foreach::foreach(i = 1:dim(data_unresolved)[1]) %do%{
+    # i=1
+
+    # Step 2.1: get all triples for unresolved quadruple
+    triples_CQ<-data_unresolved[i,c("triple1","triple2","triple3","triple4")]
+    triples_CQ = unique(unlist(triples_CQ))
+
+    # Step 2.2: check overlap with resolved triples
+    vgl1<-is.element(data_solved$triple1,triples_CQ)
+    vgl2<-is.element(data_solved$triple2,triples_CQ)
+    vgl3<-is.element(data_solved$triple3,triples_CQ)
+    vgl4<-is.element(data_solved$triple4,triples_CQ)
+
+    # Step 2.3: get resolved quadruples that contain at least one of the four tested triples
+    filt<-vgl1 | vgl2 | vgl3 | vgl4
+    data_pos<-data_solved[filt,]
+
+    # Step 2.4: search for possible fixing taxa
+    # using only taxa in data_pos, which are not in the unresolved quadruple
+    # Count the most common taxa only!
+    taxa_pos = unlist(data_pos[,c("taxa1","taxa2","taxa3","taxa4")])
+    taxa_unresolved = unique(unlist(data_unresolved[i,c("taxa1","taxa2","taxa3","taxa4")]))
+    taxa_pos2 = taxa_pos[!is.element(taxa_pos,taxa_unresolved)]
+    tab = data.table::data.table(taxa_ID = taxa_pos2)
+    tab2 = tab[,.N, by=taxa_ID]
+    tab3 = tab2[N == max(N),]
+    if(dim(tab3)[1]>1){tab3 = tab3[1,]}
+
+    # Step 2.5: return best taxa, taxa count & unresolved quadruple
+    res = data.table::data.table(unres_quad = data_unresolved[i,quadruple],
+                                 best_fixTaxa = tab3$taxa_ID,
+                                 count = tab3$N)
+    res
   }
-  for (j in c(1:dim(b)[1])){
-    if (y[j]>=4){
-      b[j,10]<-"resolved"
-    }
-  }
-  filt_res<-b$status == "resolved";
-  c<-rbind(a,b)
-  return(c)
+  myTab = data.table::rbindlist(myTab)
+  myTab
+
+  # Step 3: if count of taxa >=4 it can be considered a fixing taxon, and the quadruple is resolved
+  stopifnot(myTab$unres_quad == data_unresolved$quadruple)
+  filt = myTab$count>=4
+  data_unresolved[filt,status := "resolved"]
+  data_unresolved[filt,fixingTaxa := myTab[filt,best_fixTaxa]]
+  data_unresolved[,round := round + 1]
+  data_unresolved
+
+  # Step 4: return data
+  data3 = rbind(data_solved,data_unresolved)
+  table(data3$status)
+  tab4<-data3[,.N,by=status]
+  n_unresolved_new = tab4[status == "unresolved",N]
+  if (length(n_unresolved_new)==0){n_unresolved_new = 0}
+  n_unresolved_old = dim(data_unresolved)[1]
+  n_diff = n_unresolved_old - n_unresolved_new
+
+  if(verbose == T){message("In this round, ",n_diff," quadruples could be resolved ...")}
+  return(data3)
 }
--- R/runAlgorithm.R
+++ R/runAlgorithm.R
@@ -1,8 +1,9 @@
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param data PARAM_DESCRIPTION
-#' @return OUTPUT_DESCRIPTION
-#' @details DETAILS
+#' @title Running algorithm to find fixing taxa in a loop
+#' @description Testing the input data stepwise for fixing taxa and check if the whole set can be resolved or not
+#' @param data Data.table as constructed by create input. All possible quadruples given the taxon set with status information (quadruple as input available, quadruple not in input = unresolved). In addition, all four triples possible by each quadruple are listed.
+#' @param verbose Logical parameter if message should be printed; default F
+#' @return The same data.table is returned, with updated status, fixing taxon & round of resolvement.
+#' @details Details to the algorithm can be found in the paper LINK
 #' @examples
 #' \dontrun{
 #' if(interactive()){
@@ -11,39 +12,58 @@
 #' }
 #' @rdname runAlgorithm
 #' @export
-runAlgorithm<-function(data){
-  #data=myResults$data
-  #head(data)
+runAlgorithm<-function(data, verbose = F){
+  # data = myResults$data
+  # verbose = T
 
-  # define solved and unsolved quadruples
-  table(data$status)
-  data2<-copy(data)
+  # Step 0: check if input is ok
+  expectedNames = c("taxa1","taxa2","taxa3","taxa4","quadruple",
+                    "triple1","triple2","triple3","triple4","status")
+  stopifnot(names(data) %in% expectedNames)
+  stopifnot(class(data$taxa1) == "integer")
+
+  x = table(data$status)
+  n = data$taxa4[dim(data)[1]]
+
+  if(verbose == T){message("Using ",as.numeric(x[1])," of ",dim(data)[1], " quadruples as input for algorithm (",n," unique taxa). \n This leaves ", as.numeric(x[2])," quadruples unsolved.")}
+
+  # Step 1: Define solved and unsolved quadruples from input data
+  data2<-data.table::copy(data)
   green_quadruples<-data2[status=="input",]
   cross_quadruples<-data2[status=="unresolved",]
 
-  # In my loop I do overwrite the orignal data. Hence I want to save it here
-  alt_green<-green_quadruples
-  alt_cross<-cross_quadruples
-  all_quads<-rbind(alt_green,alt_cross)
-
-  # Start Loop
-  # check each CQ and change status if resolved
-  # repeat algorithm until all quads are resolved or all CQ have been tested
-  # in each round, at least one CQ has to be solved
-  # if index > dim, then there was at least one round in which no CQ could be solved
+  # Step 2: Start repeat loop with myAlgorithm (check for fixing taxon)
+  #   check each CQ and change status if resolved
+  #   repeat algorithm until all quads are resolved or no changes anymore
+  #   (in each round, at least one CQ has to be solved)
+  #   (if index > dim, then there was at least one round
+  #     in which no CQ could be solved)
   # then the set cannot be decisive
 
   index=0
-  repeat{all_quads=myAlgorithm(all_quads);
-  index=index+1;
-  print(index);
-  print(sum(all_quads$status == "unresolved"));
-  if ((sum(all_quads$status == "unresolved") ==0) | index >= dim(alt_cross)[1])
-    break}
+  data2[,round := 0]
+  data2[,fixingTaxa := 0]
+  repeat{
+    data2=myAlgorithm(data2,verbose = F)
+    index=index+1
+    data2
 
-  if (sum(all_quads$status == "unresolved") ==0){print("PHYLOGENETICALLY DECISIVE")}
-  if (index >= dim(alt_cross)[1]){print("NOT RESOLVABLE VIA THIS ALGORITHM, MAYBE A SECOND FIXING TAXON IS NEEDED")}
+    # Loop should stop if there are no new resolved quadruples or no unresolved quadruples
+    check1 = (sum(data2$round == index) == 0)
+    check2 = (sum(data2$status == "unresolved") == 0)
+    if (check1 | check2){
+      break
+    }
+  }
 
-  return(all_quads)
+  # Step 3: return data and result
+  if (sum(data2$status == "unresolved") ==0){
+    print("PHYLOGENETICALLY DECISIVE")
+    }
+  if (index >= dim(cross_quadruples)[1]){
+    print("NOT RESOLVABLE VIA THIS ALGORITHM, MAYBE A SECOND FIXING TAXON IS NEEDED")
+    }
+
+  return(data2)
 
 }
